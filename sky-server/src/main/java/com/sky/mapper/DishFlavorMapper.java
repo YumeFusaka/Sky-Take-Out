b5af7c518c9f96222a8f61198912431a3c030493
j <1336679971@qq.com>
2023-11-04 23:49:07 +0800

    新增菜品

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,12 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DishFlavorMapper {
+    //批量插入口味数据
+    public void insertBatch(List<DishFlavor> flavors);
+}
